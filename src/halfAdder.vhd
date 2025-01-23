ab45c68e691546beea6f1f7a54a808f4a730f06c
gral0554 <olivedog01@outlook.com>
2025-01-23 14:56:42 -0700

    ICE2

--- src/halfAdder.vhd
+++ src/halfAdder.vhd
@@ -28,7 +28,7 @@ library ieee;
 entity halfAdder is 
   port(
 	i_A     : in  std_logic; -- 1-bit input port
-	i_B     : in  std_logic; 
+	i_B     : in  std_logic;
 	o_S     : out std_logic;  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
 	-- TODO:  Carry port
 	o_Cout  : out std_logic
